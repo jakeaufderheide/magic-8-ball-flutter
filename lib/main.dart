ccfae48ce6386ee0eb4d2eff3df5f33509d6dc57
Jake Aufderheide <jakeaufderheide@gmail.com>
2020-04-06 17:40:58 -0700

    Finishes challenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,53 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Ask Me Anything',
+            style: TextStyle(fontSize: 24.0),
+          ),
+          backgroundColor: Colors.blue[900],
+        ),
+        body: MagicEightBall(),
+        backgroundColor: Colors.blue[500],
+      ),
+    ),
+  );
+}
+
+class MagicEightBall extends StatefulWidget {
+  @override
+  _MagicEightBallState createState() => _MagicEightBallState();
+}
+
+class _MagicEightBallState extends State<MagicEightBall> {
+  var ballNo = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changeEightBallImage();
+              },
+              child: Image.asset('images/ball$ballNo.png'),
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  void changeEightBallImage() {
+    setState(() {
+      ballNo = Random().nextInt(5) + 1;
+    });
+  }
+}
